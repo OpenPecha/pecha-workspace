e5e7fc4a011b004ea03162ead147fa286496519c
tenkus47 <tenkus47@gmail.com>
2025-04-10 16:18:16 +0530

    update login

--- frontend/src/page/Login.tsx
+++ frontend/src/page/Login.tsx
@@ -1,20 +1,11 @@
+import { useAuth } from "@/contexts/AuthContext";
 import React, { useEffect } from "react";
 
 function Login() {
+  const { login } = useAuth();
   useEffect(() => {
-    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-    const url = "https://" + domain + "/authorize";
-    // Construct the full authorization URL with required parameters
-    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-    const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
-    const scope = "openid profile email";
-    const responseType = "id_token token";
-
-    const authUrl = `${url}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
-
-    // Redirect the user to Auth0 login page
-    window.location.href = authUrl;
-  }, []);
+    login();
+  }, [login]);
   return <div></div>;
 }
 
