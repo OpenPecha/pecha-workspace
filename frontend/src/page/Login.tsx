5c01b12cd34e6a6a0d743db62f7b0691ff28a602
tenkus47 <tenkus47@gmail.com>
2025-04-10 16:01:17 +0530

    var to const

--- frontend/src/page/Login.tsx
+++ frontend/src/page/Login.tsx
@@ -2,19 +2,18 @@ import React, { useEffect } from "react";
 
 function Login() {
   useEffect(() => {
-    let domain=import.meta.env.VITE_AUTH0_DOMAIN;\
-    let url=domain+'/authorize'
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const url = domain + "/authorize";
     // Construct the full authorization URL with required parameters
-    let clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-    let redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
-    let scope = 'openid profile email';
-    let responseType = 'code';
-    
-    let authUrl = `${url}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
-    
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+    const scope = "openid profile email";
+    const responseType = "code";
+
+    const authUrl = `${url}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
     // Redirect the user to Auth0 login page
     window.location.href = authUrl;
-    
   }, []);
   return <div></div>;
 }
