d441973ebcb2d3518d25c864168cf11021f16d4b
tenkus47 <tenkus47@gmail.com>
2025-04-10 16:07:19 +0530

    update

--- frontend/src/page/Login.tsx
+++ frontend/src/page/Login.tsx
@@ -8,7 +8,7 @@ function Login() {
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
     const scope = "openid profile email";
-    const responseType = "code";
+    const responseType = "id_token token";
 
     const authUrl = `${url}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
 
