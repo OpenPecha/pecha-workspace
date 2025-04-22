391f4399afbc19a8ad0fc42e41e8d36ad4bad881
tenkus47 <tenkus47@gmail.com>
2025-04-22 13:31:17 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Profile from "./page/Profile";
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./page/Login";
 import Logout from "./page/Logout";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function AppContainer() {
   return (
     <BrowserRouter>
@@ -23,11 +23,15 @@ function AppContainer() {
   );
 }
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <AuthProvider>
-      <AppContainer />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppContainer />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/page/Callback.tsx
+++ frontend/src/page/Callback.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import { create_user } from "@/api/user";
+import { useMutation } from "@tanstack/react-query";
 
 const Callback = () => {
   const navigate = useNavigate();
@@ -13,37 +14,36 @@ const Callback = () => {
     getToken,
   } = useAuth();
 
+  const createUserMutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      return create_user(token!);
+    },
+    onSuccess: () => {
+      navigate("/");
+    },
+  });
+
   useEffect(() => {
-    async function handleAuth() {
-      if (isAuthenticated) {
-        const token = await getToken();
-        // Navigate to the home page after a short delay
-        create_user(token!).then(() => navigate("/"));
-      }
-    }
-    // Auth0 will handle the token processing automatically
-    // We just need to redirect once authentication is complete
     if (isAuthenticated) {
-      // Navigate to the home page after a short delay
-      handleAuth();
+      createUserMutation.mutate();
     }
   }, [isAuthenticated]);
 
   useEffect(() => {
-    // If there's an error from Auth0, capture it
     if (auth0Error?.message.includes("login_required")) {
       console.log("Silent login failed: user must log in manually.");
       login(false);
     }
   }, [isLoading, auth0Error, navigate, login]);
 
-  // Determine which content to display based on the current state
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          redirecting login...
+          {createUserMutation.isPending
+            ? "Processing login..."
+            : "Redirecting..."}
         </div>
       </div>
     </div>
