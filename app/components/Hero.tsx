8f510f65eb76a9e40e7bb50a128b731ee986e083
tenkus47 <tenkus47@gmail.com>
2025-09-10 11:43:48 +0530

    Update Hero component to replace text span with an h1 element for improved semantic structure and accessibility.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -62,7 +62,7 @@ const Hero = () => {
               >
         <div className="flex flex-col text-center mt-4 gap-4 relative z-10 px-4">
          <span className="text-sm text-gray-500">A platform to manage </span>
-         <span className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-tight">Powering Buddhist AI. Together</span>
+         <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-tight">Powering Buddhist AI. Together</h1>
         </div>
         <div className="flex justify-center p-4 sm:p-8 md:p-12 lg:p-20 mt-6 sm:mt-8 md:mt-10 relative z-10">
          <div className="relative w-full max-w-5xl">
